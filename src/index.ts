605094cbdd45f403193523e2f98481de040c9f66
Abhimanyu08 <iamabhimanyu08@gmail.com>
2026-07-09 14:58:12 +0530

    initial setup

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { Hono } from "hono";
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/node-postgres";
+
+const app = new Hono();
+
+const db = drizzle(process.env.DATABASE_URL!);
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+export default app;
